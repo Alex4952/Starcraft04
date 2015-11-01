6e689aadc95077ef32d3df6303453d03445e1ed3
Alex4952 <keyzoo@gmail.com>
2015-11-01 10:13:32 +0900

    Initial Commit

---
+++ Starcraft04/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Starcraft04
+//
+//  Created by GIJU HONG on 11/1/15.
+//  Copyright © 2015 GIJU HONG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
